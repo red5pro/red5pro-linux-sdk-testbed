b85291ad513d297765f1eba40d65413434e325ab
bustardcelly <bustardcelly@gmail.com>
2020-04-07 16:12:06 -0400

    proper include of headers.

--- testbed.c
+++ testbed.c
@@ -20,7 +20,7 @@
 //  WHETHER IN  AN  ACTION  OF  CONTRACT,  TORT  OR  OTHERWISE,  ARISING  FROM,  OUT  OF  OR  IN CONNECTION 
 //  WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-#include "librprosdk/include/r5pro_api.h"
+#include "librprosdk/r5pro_api.h"
 #include <dlfcn.h>
 #include <stdio.h>
 #include <stdlib.h>
